28eee02602e10108979bac7809f61c91db356b7a
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-18 03:39:01 +0530

    updated

---
+++ data.js
@@ -0,0 +1,45 @@
+export default electronics=[
+    {
+        fertilizerId:1,
+        name:"bio phos",
+        price:"300.00",
+    },
+    {
+        fertilizerId:2,
+        name:"bio anything",
+        price:"400.00",
+    },
+    {
+        fertilizerId:3,
+        name:"bio blaaaaaaaaa",
+        price:"500.00",
+    },
+    {
+        fertilizerId:4,
+        name:"bio vermi",
+        price:"600.00",
+    },
+]
+
+export const books=[
+    {
+        fertilizerId:1,
+        name:"bio phoscategories",
+        price:"308.00",
+    },
+    {
+        fertilizerId:1,
+        name:"book boarersfdngnv",
+        price:"330.00",
+    },
+    {
+        fertilizerId:1,
+        name:"books phos",
+        price:"340.00",
+    },
+    {
+        fertilizerId:1,
+        name:"booasdwefw phos",
+        price:"350.00",
+    }
+]
