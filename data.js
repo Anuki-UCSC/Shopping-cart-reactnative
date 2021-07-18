575ea2b2c4a676370238d0609b091b6df509c2fa
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-07-18 11:15:42 +0530

    data formatted

--- data.js
+++ data.js
@@ -1,44 +1,44 @@
-export default electronics=[
+export const electronics=[
     {
-        fertilizerId:1,
+        fertilizerId:'1',
         name:"bio phos",
         price:"300.00",
     },
     {
-        fertilizerId:2,
+        fertilizerId:'2',
         name:"bio anything",
         price:"400.00",
     },
     {
-        fertilizerId:3,
+        fertilizerId:'3',
         name:"bio blaaaaaaaaa",
         price:"500.00",
     },
     {
-        fertilizerId:4,
+        fertilizerId:'4',
         name:"bio vermi",
         price:"600.00",
-    },
+    }
 ]
 
 export const books=[
     {
-        fertilizerId:1,
+        fertilizerId:'1',
         name:"bio phoscategories",
         price:"308.00",
     },
     {
-        fertilizerId:1,
+        fertilizerId:'2',
         name:"book boarersfdngnv",
         price:"330.00",
     },
     {
-        fertilizerId:1,
+        fertilizerId:'3',
         name:"books phos",
         price:"340.00",
     },
     {
-        fertilizerId:1,
+        fertilizerId:'4',
         name:"booasdwefw phos",
         price:"350.00",
     }
